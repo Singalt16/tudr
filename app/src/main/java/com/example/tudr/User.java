9291d96ebd95f6da5afe38001da1ad6a2d9a6b64
Singalt16 <singalt16@gmail.com>
2019-09-28 12:01:28 -0400

    added user class

---
+++ app/src/main/java/com/example/tudr/User.java
@@ -0,0 +1,33 @@
+package com.example.tudr;
+import java.util.ArrayList;
+
+public class User {
+
+    public User(String id) {
+
+    }
+
+    public ArrayList<Integer> getRoles() {
+
+    }
+
+    public ArrayList<String> getClasses() {
+
+    }
+
+    public String getImage() {
+
+    }
+
+    public void addClass() {
+
+    }
+
+    public void removeClass() {
+
+    }
+
+    public void setImage() {
+
+    }
+}
