e323c63abc014b4a635e4e47ac371b67e83b7355
Singalt16 <singalt16@gmail.com>
2019-09-28 12:03:07 -0400

    changed activity name to user profile

--- app/src/main/java/com/example/tudr/UserProfile.java
+++ app/src/main/java/com/example/tudr/UserProfile.java
@@ -3,11 +3,13 @@ package com.example.tudr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class UserProfile extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.user_profile);
     }
 }
